7469d1ddbb267cb908665ef20a2e5bfa65caf419
Juan Prenss <juanthefgr@gmail.com>
2023-07-09 21:00:34 -0500

    Subida

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { Route, RouterModule } from '@angular/router';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { AboutPagesComponent } from './shared/pages/about-pages/about-pages.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
+import { CountriesModule } from './countries/countries.module';
+
+
+const routes: Route[] = [
+  // {
+  //   path: '',
+  //   component: HomePageComponent
+  // },
+  {
+    path: 'about',
+    component: AboutPagesComponent
+  },
+  {
+    path: 'contact',
+    component: ContactPageComponent
+  },{
+  path: 'countries',
+  loadChildren: ()=> import('./countries/countries.module').then( m=> m.CountriesModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'countries'
+  }
+];
+
+
+@NgModule({
+  imports:[
+      RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule]
+})
+export class AppRoutingMosule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPagesComponent } from './pages/about-pages/about-pages.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    HomePageComponent,
+    AboutPagesComponent,
+    SidebarComponent,
+    ContactPageComponent,
+    SearchBoxComponent,
+  ],
+  imports: [
+    CommonModule, RouterModule
+  ],
+  exports:[
+    HomePageComponent,
+    AboutPagesComponent,
+    SidebarComponent,
+    ContactPageComponent,
+    SearchBoxComponent]
+})
+export class SharedModule { }
